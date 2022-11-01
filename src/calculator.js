39b7e0957456dbaa9bde42dc557e419779a8e8c0
Miquel de Domingo <miquelddg@gmail.com>
2022-11-01 18:54:55 +0100

    fix: fix `multiply` and `divide`

--- src/calculator.js
+++ src/calculator.js
@@ -6,10 +6,10 @@ const Calculator = {
     return values.reduce((prev, curr) => prev - curr, 0);
   },
   multiply(...values) {
-    return values.reduce((prev, curr, i) => (i === 0 ? curr : prev * curr));
+    return values.reduce((prev, curr) => prev * curr);
   },
   divide(...values) {
-    return values.reduce((prev, curr, i) => (i === 0 ? curr : prev / curr));
+    return values.reduce((prev, curr) => prev / curr);
   },
   mod(...values) {
     return values.reduce((prev, curr) => prev % curr);
